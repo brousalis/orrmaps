c127aa8f334aa34c65f748dac477d0056f631d12
brousalis <brousapg@gmail.com>
2012-10-07 17:32:56 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orrore::Application.config.secret_token = 'e33a2693c513dd8d179ba7f9fc0b66573dcabab661fb110b0b16c7e1ad86755bbe7a975ff395b0ec62bd36b9144eff7041e0f866a2574328737bf278f2f71a98'
