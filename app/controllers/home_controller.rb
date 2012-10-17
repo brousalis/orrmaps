1eee8a63ecd58dd20a4aa1c14626cfaab306e39f
brousalis <brousapg@gmail.com>
2012-10-16 20:10:40 -0500

    silly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,13 @@ class HomeController < ApplicationController
     @user = User.new unless current_user
     @map = Map.new unless current_user
 
-    server = Server.find_by_name(session[:server]) if session[:server]
-    server = Server.find_by_name("Borlis Pass") if !session[:server]
-    @rated = Map.find_all_by_server_id(server.id).collect{|m|{:map=>m,:count=>m.likes.count}}.sort{|a,b| b[:count] <=> a[:count]}
+    @server = Server.find_by_name(session[:server]) if session[:server]
+    @server = Server.find_by_name("Borlis Pass") if !session[:server]
+    @rated = Map.find_all_by_server_id(@server.id).collect{|m|{:map=>m,:count=>m.likes.count}}.sort{|a,b| b[:count] <=> a[:count]}
 
     if current_user
       if !current_user.map
-        map = Map.new(:user => current_user, :server => server)
+        map = Map.new(:user => current_user, :server => @server)
         user = current_user
         map.save
         user.map = map
