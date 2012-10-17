9f2d1b7252b6b68261f20d1b29d4cd5a42f09219
brousalis <brousapg@gmail.com>
2012-10-16 20:09:33 -0500

    OMG PROD ISSUES

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
-  before_filter :default_server
-
   def index
     @user = User.new unless current_user
     @map = Map.new unless current_user
-    server = Server.find_by_name(session[:server])
+
+    server = Server.find_by_name(session[:server]) if session[:server]
+    server = Server.find_by_name("Borlis Pass") if !session[:server]
     @rated = Map.find_all_by_server_id(server.id).collect{|m|{:map=>m,:count=>m.likes.count}}.sort{|a,b| b[:count] <=> a[:count]}
 
     if current_user
@@ -18,11 +18,5 @@ class HomeController < ApplicationController
       end
       @map = current_user.map
     end
-  end
-
-  def default_server
-    session[:server] = current_user.server.name if current_user
-    session[:server] ||= "Borlis Pass" unless current_user
-    @server = session[:server]
   end
 end
