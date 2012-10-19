4c228e642c14a210cccb1289feddf90900d878e7
brousalis <brousapg@gmail.com>
2012-10-19 00:31:42 -0500

    Stupid

--- db/migrate/20121019043340_upvotes.rb
+++ db/migrate/20121019043340_upvotes.rb
@@ -1,7 +1,6 @@
 class Upvotes < ActiveRecord::Migration
   def up
     add_column :points, :votes, :integer, :null => false, :default => 5
-    add_column :points, :icon, :strong
   end
 
   def down
