1107642eb106ca94df325b6eeefe0a2aa7761d94
David Pick <pickdavid@gmail.com>
2012-10-28 20:37:04 -0500

    removing some duplication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,17 +8,13 @@ module ApplicationHelper
   end
 
   def points_for_map(map)
-    @map_ids_to_point_count ||= Rails.cache.fetch("maps/points", :expires_in => 15.minutes) do
-      cache_counts(Point)
-    end
+    @map_ids_to_point_count ||= cache_counts(Point)
 
     @map_ids_to_point_count[map.id] || 0
   end
 
   def likes_for_map(map)
-    @map_ids_to_like_count ||= Rails.cache.fetch("maps/likes", :expires_in => 15.minutes) do
-      cache_counts(Like)
-    end
+    @map_ids_to_like_count ||= cache_counts(Like)
 
     @map_ids_to_like_count[map.id] || 0
   end
@@ -32,8 +28,10 @@ module ApplicationHelper
   private
 	
   def cache_counts(rel)
-    rel.select("map_id, COUNT(*) as count").group('map_id').inject({}) do |cache, item|
-      cache.merge(item.map_id => item.count.to_i)
+    Rails.cache.fetch("maps/#{rel.downcase}", :expires_in => 15.minutes) do
+      rel.select("map_id, COUNT(*) as count").group('map_id').inject({}) do |cache, item|
+        cache.merge(item.map_id => item.count.to_i)
+      end
     end
   end
 end
