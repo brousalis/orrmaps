ff95d288f7f2798944474454e19ebb007c21cfd8
David Pick <pickdavid@gmail.com>
2012-11-08 00:15:24 -0600

    fixing redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,3 @@ Orrore::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
-
-$redis = Redis.new(:host => "localhost", :port => "6379")
-
